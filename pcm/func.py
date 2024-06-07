0e4fe3f9c6c86e549574a23231e77eaeaa7e01fa
jirhiker <jirhiker@users.noreply.github.com>
2024-06-07 15:01:10 +0000

    Formatting changes

--- pcm/func.py
+++ pcm/func.py
@@ -166,11 +166,11 @@ def _scripts(env, use_ngx, overwrite, verbose):
     post_m_args = "post_measurement", "post_measurement"
 
     for name, filename in (
-            measurement_args,
-            extraction_args,
-            procedure_args,
-            post_eq_args,
-            post_m_args,
+        measurement_args,
+        extraction_args,
+        procedure_args,
+        post_eq_args,
+        post_m_args,
     ):
         _render_template(
             (root, "scripts", name), "example_{}.py".format(filename), overwrite
@@ -186,19 +186,19 @@ def _setupfiles(env, use_ngx, overwrite, verbose):
     sf = util.r_mkdir(root, "setupfiles")
 
     for d, ps, enabled in (
-            ("canvas2D", ("canvas.yaml", "canvas_config.xml", "alt_config.xml"), True),
-            ("extractionline", ("valves.yaml",), True),
-            ("monitors", ("system_monitor.cfg",), True),
+        ("canvas2D", ("canvas.yaml", "canvas_config.xml", "alt_config.xml"), True),
+        ("extractionline", ("valves.yaml",), True),
+        ("monitors", ("system_monitor.cfg",), True),
+        (
+            "devices",
             (
-                    "devices",
-                    (
-                            "ngx_switch_controller.cfg",
-                            "spectrometer_microcontroller.cfg",
-                            "NGXGPActuator.cfg",
-                    ),
-                    use_ngx,
+                "ngx_switch_controller.cfg",
+                "spectrometer_microcontroller.cfg",
+                "NGXGPActuator.cfg",
             ),
-            ("", ("startup_tests.yaml", "experiment_defaults.yaml"), True),
+            use_ngx,
+        ),
+        ("", ("startup_tests.yaml", "experiment_defaults.yaml"), True),
     ):
         if d:
             # out = os.path.join(sf, d)
@@ -246,7 +246,7 @@ def _code(fork, branch, app_id):
 
 
 def _launcher(
-        conda, environment, app, org, app_id, login, msv, output, overwrite, verbose
+    conda, environment, app, org, app_id, login, msv, output, overwrite, verbose
 ):
     click.echo("launcher")
 
@@ -335,12 +335,12 @@ def _init(env, org, data_org, use_ngx, overwrite, verbose):
     }
 
     for template, ctx, flag in (
-            ("general.ini", gctx, True),
-            ("dvc.ini", {"org": data_org}, True),
-            ("update.ini", uctx, True),
-            ("arar_constants.ini", {}, True),
-            ("extractionline.ini", ectx, True),
-            ("ngx.ini", {}, use_ngx),
+        ("general.ini", gctx, True),
+        ("dvc.ini", {"org": data_org}, True),
+        ("update.ini", uctx, True),
+        ("arar_constants.ini", {}, True),
+        ("extractionline.ini", ectx, True),
+        ("ngx.ini", {}, use_ngx),
     ):
         if flag:
             txt = render.render_template(template, **ctx)
@@ -447,5 +447,6 @@ def _req():
     # pip
     p = " ".join(PIP_REQUIREMENTS)
     click.secho(f">>>>> pip install {p}", fg="green")
+
 
 # ============= EOF =============================================
